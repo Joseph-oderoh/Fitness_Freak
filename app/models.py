5d226b46056a1ebd1a7eea8e745613d55ea67ed9
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-17 12:55:44 +0300

    initall  commit

---
+++ app/models.py
@@ -0,0 +1,56 @@
+class User(UserMixin ,db.Model):
+    """
+    """
+    __tablename__='users'
+    id= db.Column(db.Integer, primary_key=True) 
+    username = db.Column(db.String , unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    bio = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    post = db.relationship('Post', backref='user', lazy = 'dynamic')
+    
+    
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+
+class Post(db.Model):
+    __tablename__='posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.String())
+    date_created=db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+    comment = db.relationship('Comment',backref='post' ,lazy='dynamic' )
+    
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+    def repr(self):
+        return f'Post {self.title}' 
+
+
+class Comment(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    date_created= db.Column(db.DateTime,default=datetime.utcnow)
+    blog_id = db.Column(db.Integer,db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'Comment {self.name}'         
