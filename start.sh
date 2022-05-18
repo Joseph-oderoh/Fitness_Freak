80bdca36943d7d37a503c046805954d7c3e2867f
carol-profile <carolgitonga93@gmail.com>
2022-05-19 00:52:56 +0300

    add start.sh file

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME='carolgitonga93@gmail.com'
+export MAIL_PASSWORD='lorac@15'
+export SECRET_KEY='secret'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carol:lorac1234@localhost/nutrition'
+
+python3 manage.py server
