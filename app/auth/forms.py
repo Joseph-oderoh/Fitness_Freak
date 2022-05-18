96f137698a8592e93b89fe1b2485ae32986c2f9e
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-05-18 23:24:00 +0300

    add auth routing

---
+++ app/auth/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField, BooleanField,SelectField,ValidationError
+from wtforms.validators import DataRequired,Email,EqualTo, InputRequired, Email, Length
+from ..models import User
+
+class RegistrationForm(FlaskForm):
+    firstname = StringField('First Name',validators=[DataRequired()])
+    lastname = StringField('Last Name',validators=[DataRequired()])
+    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+    username = StringField('Enter your username',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Password',validators = [DataRequired()])
+    terms = BooleanField('I have read and agreed with the terms of service and <br> our privacy policy.')
+    submit = SubmitField('SIGN UP')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Email(), Length(1, 64)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=72)])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,28 +1,45 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField, BooleanField,SelectField,ValidationError
-from wtforms.validators import DataRequired,Email,EqualTo, InputRequired, Email, Length
+from . import auth
+from flask import render_template, redirect, url_for, flash, request
 from ..models import User
+from .forms import RegistrationForm, LoginForm
+from .. import db
+from flask_login import login_user, logout_user, login_required
 
-class RegistrationForm(FlaskForm):
-    firstname = StringField('First Name',validators=[DataRequired()])
-    lastname = StringField('Last Name',validators=[DataRequired()])
-    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
-    username = StringField('Enter your username',validators = [DataRequired()])
-    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Password',validators = [DataRequired()])
-    terms = BooleanField('I have read and agreed with the terms of service and <br> our privacy policy.')
-    submit = SubmitField('SIGN UP')
 
-    def validate_email(self,data_field):
-            if User.query.filter_by(email =data_field.data).first():
-                raise ValidationError('There is an account with that email')
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(firstname=form.firstname.data,
+                    lastname=form.lastname.data,
+                    email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
 
-    def validate_username(self,data_field):
-        if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is taken')
+    title = "Blog | Sign In"
+    return render_template('auth/register.html', registration_form=form, title=title)
 
-class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[InputRequired(), Email(), Length(1, 64)])
-    password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=72)])
-    remember = BooleanField("Remember Me")
-    submit = SubmitField("Login")
+
+# Login route
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(url_for("home.homepage"))
+        
+        flash("Invalid username or password")
+        
+    title = "Login"
+    return render_template("auth/login.html", login_form = login_form, title = title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
