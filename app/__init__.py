f2ce79af14358e50c8d5e25879438bb636f5a4f6
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-18 13:05:18 +0300

    Create instances of Bootsrap, SQLAlchemy and login manager classes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
+from ensurepip import bootstrap
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config_options
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
