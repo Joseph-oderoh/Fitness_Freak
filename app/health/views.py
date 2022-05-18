32e379834b09da51c898d2c56211ffa6eb815bba
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-18 10:25:19 +0300

    add style to the home page

--- app/health/views.py
+++ app/health/views.py
@@ -3,4 +3,4 @@ from flask import render_template
 @health.route('/')
 def index():
     title = 'Health and Wellness'
-    return render_template('health.html', title=title)
+    return render_template('index.html', title=title)
