debd826d887425b65d8fbabf83fbfdd041b1d035
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-19 05:10:42 +0300

    Create form to add post.

--- app/fitness/forms.py
+++ app/fitness/forms.py
@@ -1,10 +1,18 @@
 
 from flask_wtf import FlaskForm
 from wtforms.validators import InputRequired,Length,EqualTo
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField, SelectField, RadioField
 
 class CommentForm(FlaskForm):
   content = TextAreaField('Leave a Comment.',validators=[InputRequired()])
   submit = SubmitField ('Submit')
 
+class FormPost(FlaskForm):
+  title = StringField('Title', validators=[InputRequired()])
+  content = TextAreaField('Post', validators=[InputRequired()])
+  category = RadioField('Category :', choices = [('Fitness', 'Fitness')], validators = [InputRequired()])
+  # category = SelectField('Category',choices=[('Health','Health'),('Fitness','Fitness'),('Nutrition','Nutrition')])
+  author = StringField('Author')
   
+  submit = SubmitField('Add Post')
+
