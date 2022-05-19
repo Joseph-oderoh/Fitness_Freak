f6b233debe087b696f3d15e09c3485ab105d5ab6
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-19 05:21:03 +0300

    Add index function to render index.html file with the posts.

--- app/fitness/views.py
+++ app/fitness/views.py
@@ -4,3 +4,8 @@ from .. models import User, Comment, Post
 from .forms import PostForm, CommentForm
 from flask_login import login_required, current_user
 from flask import  render_template,redirect, request,url_for,abort,flash
+@fitness.route('/')
+def index():
+    title = 'Fitness'
+    post = Post.query.order_by(Post.date_created).all()
+    return render_template('index.html', post = post, title=title)
