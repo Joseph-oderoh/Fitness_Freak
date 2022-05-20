33c722b12670d3be8c4a6dbe6d7c8163d63d01a6
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-20 04:02:40 +0300

    deployment

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export SECRET_KEY='powerfulsecretkey'
-export DATBASE_URL='postgres://agiwurekducacm:39484a4456677ff94b5426ea89ed827139ee1c8f485d3ac7827e979aea1e3b20@ec2-34-236-94-53.compute-1.amazonaws.com:5432/d8v5mj5hkqnc0n'
+export DATBASE_URL='postgres://tamijojicrypsm:774c332c43f35f0a9aae9c86abe0a50c7725a296aeb708deb895f71420ce2eae@ec2-54-165-184-219.compute-1.amazonaws.com:5432/d6cjgg677563ma'
 python3.8 manage.py server
